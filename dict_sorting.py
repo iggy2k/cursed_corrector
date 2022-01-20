bcb4d5c5825dec238047f5cde592715a30a97646
Ali Al-Hasnawi <44278853+alialhasnawi@users.noreply.github.com>
2022-01-20 07:20:46 -0500

    Shifted line indices

--- dict_sorting.py
+++ dict_sorting.py
@@ -1,16 +1,23 @@
 words = []
 with open('words_alpha.txt', 'r') as fin:
     for line in fin:
-        words.append(line.replace('\n', ''))
+        words.append(line.strip())
 words.sort(key=len)
 
 idx = 1
 
 with open('words_sorted.txt', 'w') as fout:
-    for i, word in enumerate(words):
+    length_line_seps = []
+
+    # Build the first line of the file, which contains line numbers of where
+    # to find words of a specific length. 
+    for i, word in enumerate(words): 
         if len(word) == idx:
-            fout.write(str(i + 2) + ', ')
+            length_line_seps.append(str(i))
             idx += 1
+
+    fout.write(','.join(length_line_seps))
     fout.write('\n')
+
     for word in words:
         fout.write(word + '\n')
